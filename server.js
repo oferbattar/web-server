7a7e441c15bd9f0bc27ffe001c7e2e81987591a7
Ofer Ben Attar <oferbattar@gmail.com>
2016-04-27 19:10:41 +0300

    update port for heroku

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 var express = require('express');
 var app = express();
-var middleware ={
-	reqireAuthentication: function(req , res, next){
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req, res, next){
-		
-		console.log('Request: ' + req.method +' ' + req.originalUrl+ ' ' +new Date().toString());
-		next();
-	}
-};
+var middleware= require('./middleware.js');
+var PORT = process.env.PORT || 3000;
 app.use(middleware.logger);
 app.get('/about',middleware.reqireAuthentication, function(req,res){
 res.send('about us!');	
 });
 
 app.use(express.static(__dirname +'/public'));
-app.listen(3000,function(){
+app.listen(PORT,function(){
 	console.log('server is running');
 });
